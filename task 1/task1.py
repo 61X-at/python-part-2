61dabaaa620df147e88e8052a4fd995fd226396e
Andrew 61X Tiunov <astermiizek@gmail.com>
2023-12-13 22:56:58 +0500

    ``Tasks 1 - 5``

---
+++ task 1/task1.py
@@ -0,0 +1,21 @@
+from Square import Square
+
+def get_nums_square(count):
+    for num in range(1, count + 1):
+        yield num ** 2
+
+
+class_iterator = Square(10)
+generator = get_nums_square(10)
+square_nums = (num ** 2 for num in range(1, 11))
+
+for my_num in class_iterator:
+    print(my_num)
+print()
+
+for my_num in generator:
+    print(my_num)
+print()
+
+for my_num in square_nums:
+    print(my_num)
---
+++ task2/task2.py
@@ -0,0 +1,13 @@
+import os.path
+
+def gen_files_path(path: str = os.path.join('..')) -> list[str]:
+    result = list()
+    for root, dirs, files in os.walk(path):
+        result.extend([os.path.join(root, file_name) for file_name in files])
+    return result
+
+
+print(gen_files_path())
+
+user_path = input('Укажите путь до каталога: ')
+print(gen_files_path(user_path))
